28fa9bfffec3dbcace2469fd85060a477620dd52
movilla <moviglez@gmail.com>
2016-06-27 19:51:48 +0200

    portada de cinco a diez juegadores

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+var cinco =  document.getElementById('jug_5')
+if (cinco) {
+        document.querySelector('#jug_5').onclick = function () {
+                var nuevo = confirm('¿Quieres iniciar el juego con cinco jugadores?');
+                if (nuevo == true) { 
+                        location.href = 'index_5.html'
+                }
+        };
+}
+
+
 var nodeList = document.getElementsByClassName('draggable');
 
 for(var i=0;i<nodeList.length;i++) {
@@ -14,21 +25,28 @@ for(var i=0;i<nodeList.length;i++) {
   }, false);
 };
 
-document.querySelector('#b01a').onclick = function () {
-        var audio = confirm('¿Quieres comenzar con la narración?');
-        if (audio == true) {
-                document.getElementById('sonido_1').play();
-                var activar = document.getElementById('b01a');
-                activar.style.visibility ='hidden';  
-                document.querySelector('#b02a').style.visibility ='visible';
-        }
-        else { 
-                alert('Cancelado')
-        }
-};
 
-document.querySelector('#b02a').onclick = function () {
-                document.getElementById('sonido_1').pause();
-                document.querySelector('#b02a').style.visibility ='hidden';
-                document.querySelector('#b01a').style.visibility ='visible';    
-};
+var ba =  document.getElementById('b01a')
+if (ba) {
+        document.querySelector('#b01a').onclick = function () {
+                var audio = confirm('¿Quieres comenzar con la narración?');
+                if (audio == true) {
+                        document.getElementById('sonido_1').play();
+                        var activar = document.getElementById('b01a');
+                        activar.style.visibility ='hidden';  
+                        document.querySelector('#b02a').style.visibility ='visible';
+                }
+                else { 
+                        alert('Cancelado')
+                }
+        };
+}
+
+var bb =  document.getElementById('b02a')
+if (bb) {
+        document.querySelector('#b02a').onclick = function () {
+                        document.getElementById('sonido_1').pause();
+                        document.querySelector('#b02a').style.visibility ='hidden';
+                        document.querySelector('#b01a').style.visibility ='visible';    
+        };
+}
